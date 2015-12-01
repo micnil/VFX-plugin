27d694c0b973e927fdb06d76970f3668acf7a755
micnil <michaelnilsson90@hotmail.com>
2015-12-01 12:49:06 +0100

    createboids, clear and run

--- flock.py
+++ flock.py
@@ -1,17 +1,49 @@
 import maya.cmds as cmds
+import boid.Boid as Boid
+import random
 
 boids = []
 
-def createBoids(numBoids) #create numboids boids and randomize position
+def createBoids(numBoids):
+	'''create numboids boids and randomize position'''
+	for i in range(numBoids):
+		boid = Boid("boid{0}".format(i))
+		x = random.uniform(-10, 10);
+		y = random.uniform(-10, 10);
+		z = random.uniform(-10, 10);
+		boid.setPosition(x, y, z)
+		boids.append(Boid("boid{0}".format(i)))
 
-def run() #run the simulation
+	print 'creating boids done'
 
-def clear() #cleanup
 
-def createKeyFrames(numFrames) #create the keyframes for the animation
+def run():
+	'''run the simulation'''
+	createBoids()
+	createKeyFrames(100)
 
-def alignment() #flocking function
+	cmds.playbackOptions(max=nFrames)
+	cmds.playbackOptions(aet=nFrames)
 
-def separation() #flocking function
+	cmds.play()
 
-def cohesion() #flocking function
+def clear():
+	'''cleanup'''
+	while boids:
+		boid = boids.pop()
+		boid.delete()
+
+def createKeyFrames(numFrames):
+	'''create the keyframes for the animation'''
+	pass
+def alignment():
+	'''flocking function'''
+	pass
+def separation():
+	'''flocking function'''
+	pass
+def cohesion():
+	'''flocking function'''
+	pass
+
+
