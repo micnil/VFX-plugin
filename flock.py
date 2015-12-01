073098a9d33b62d6d3f772eba39bb1d146d0cbed
micnil <michaelnilsson90@hotmail.com>
2015-12-01 21:46:08 +0100

    added vector lib, added acceleration of boids, orientation of movement correct

--- flock.py
+++ flock.py
@@ -1,47 +1,52 @@
 import maya.cmds as cmds
-import boid.Boid as Boid
+from boid import Boid
+from vectors import *
 import random
+import math
 
 boids = []
-dt=1/60
+dt=1/100.0
 
 def createBoids(numBoids):
 	'''create numboids boids and randomize position'''
 	for i in range(numBoids):
-		boid = Boid("boid{0}".format(i))
-		x = random.uniform(-10, 10);
-		y = random.uniform(-10, 10);
-		z = random.uniform(-10, 10);
-		boid.setPosition(x, y, z)
-		boids.append(Boid("boid{0}".format(i)))
+		b = Boid("boid{0}".format(i))
+		x = random.uniform(-5, 5);
+		y = random.uniform(-5, 5);
+		z = random.uniform(-5, 5);
+		b.setPosition(x, y, z)
+		boids.append(b)
 
 	print 'creating boids done'
+
+def clear():
+	'''cleanup'''
+	while boids:
+		print boid._name
+		b = boids.pop()
+		b.delete()
+
+def createKeyFrames(numFrames):
+	'''create the keyframes for the animation'''
+	for frame in range(numFrames):
+		cmds.currentTime( frame, edit=True )
+		for b in boids:
+			# example force (just until we get the boid rules right)
+			b.addForce(V(-1.0, math.sin(math.radians(frame)), 0.5))
+			b.move(dt)
+			b.setKeyFrame(frame)
 
 
 def run():
 	'''run the simulation'''
-	createBoids()
-	createKeyFrames(100)
+	nFrames = 500;
+	createBoids(3)
+	createKeyFrames(nFrames)
 
 	cmds.playbackOptions(max=nFrames)
 	cmds.playbackOptions(aet=nFrames)
 
 	cmds.play()
-
-def clear():
-	'''cleanup'''
-	while boids:
-		boid = boids.pop()
-		boid.delete()
-
-def createKeyFrames(numFrames):
-	'''create the keyframes for the animation'''
-	for frame in range(numFrames)
-		for boid in boids
-			boid.addVelocity([0.01, 0, 0])
-			boid.move(dt)
-			boid.setKeyframe(frame*dt)
-
 
 def alignment():
 	'''flocking function'''
