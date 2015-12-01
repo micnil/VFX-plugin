604fde95b161f19cd1d3a3f363c0556a34babb56
Martin <marki423@student.liu.se>
2015-12-01 11:22:48 +0100

    some initial structure

---
+++ boid.py
@@ -0,0 +1,10 @@
+import maya.cmds as cmds
+
+class Boid():
+	_name = ""
+
+def setPosition()
+
+def getPosition()
+
+def move()
---
+++ flock.py
@@ -0,0 +1,17 @@
+import maya.cmds as cmds
+
+boids = []
+
+def createBoids(numBoids) #create numboids boids and randomize position
+
+def run() #run the simulation
+
+def clear() #cleanup
+
+def createKeyFrames(numFrames) #create the keyframes for the animation
+
+def alignment() #flocking function
+
+def separation() #flocking function
+
+def cohesion() #flocking function
